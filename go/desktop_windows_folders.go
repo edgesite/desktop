2b97de0ba92b62da4fb6817398f4f91a34d7d8bb
Alexey Kuznetsov <axet@me.com>
2015-09-15 00:54:45 +0300

    know path

--- go/desktop_windows_folders.go
+++ go/desktop_windows_folders.go
@@ -29,24 +29,27 @@ func getDownloadsFolder() string {
 		// http://stackoverflow.com/questions/7672774/how-do-i-determine-the-windows-download-folder-path
 		//
 		var guid GUID = GUIDNew("374DE290-123F-4565-9164-39C4925E467B")
-
-		var pszPath uintptr
-		hResult, _, _ := SHGetKnownFolderPath.Call(Ptr(&guid), Ptr(SHGFP_TYPE_CURRENT), Ptr(0), Ptr(&pszPath))
-		switch hResult {
-		case S_FILE_NOT_FOUND:
-			panic("File not Found")
-		case S_OK:
-			path := WString2String(pszPath)
-			CoTaskMemFree.Call(pszPath)
-			return path
-		default:
-			panic(fmt.Sprint("Error: %x", hResult))
-		}
+		return knowpath(guid)
 	}
 }
 
 func getDesktopFolder() string {
 	return path(CSIDL_DESKTOPDIRECTORY)
+}
+
+func knowpath(guid GUID) string {
+	var pszPath uintptr
+	hResult, _, _ := SHGetKnownFolderPath.Call(Ptr(&guid), Ptr(SHGFP_TYPE_CURRENT), Ptr(0), Ptr(&pszPath))
+	switch hResult {
+	case S_FILE_NOT_FOUND:
+		panic("File not Found")
+	case S_OK:
+		path := WString2String(pszPath)
+		CoTaskMemFree.Call(pszPath)
+		return path
+	default:
+		panic(fmt.Sprint("Error: %x", hResult))
+	}
 }
 
 func path(nFolder int) string {
