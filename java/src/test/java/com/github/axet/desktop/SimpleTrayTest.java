8e1bc345c302e79b25bc735cc277b97517f8cb78
Alexey Kuznetsov <axet@me.com>
2015-09-22 20:20:23 +0300

    add daemon thread mac

--- java/src/test/java/com/github/axet/desktop/SimpleTrayTest.java
+++ java/src/test/java/com/github/axet/desktop/SimpleTrayTest.java
@@ -5,20 +5,14 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
-import net.sf.image4j.codec.ico.ICODecoder;
-
-public class SimpleTrayTest extends JFrame {
-    private static final long serialVersionUID = 1L;
-
+public class SimpleTrayTest {
     DesktopSysTray sys = Desktop.getDesktopSysTray();
     JPopupMenu menu;
 
@@ -32,7 +26,6 @@ public class SimpleTrayTest extends JFrame {
         public void mouseLeftDoubleClick() {
             System.out.println("double click");
         }
-
     };
 
     ImageIcon loadImage(String s) {
@@ -46,36 +39,47 @@ public class SimpleTrayTest extends JFrame {
     }
 
     public SimpleTrayTest() {
-        super("MainFrame");
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final Icon icon1 = loadImage("icon.png");
+        final Icon icon2 = loadImage("icon2.png");
 
         menu = new JPopupMenu();
-        JMenuItem menuItem1 = new JMenuItem("test1");
+        JMenuItem menuItem1 = new JMenuItem("icon1");
+        menuItem1.setIcon(icon1);
         menuItem1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                System.out.println("test1");
+                sys.setIcon(icon1);
+                System.out.println("icon1");
             }
         });
         menu.add(menuItem1);
         menu.addSeparator();
-        JMenuItem menuItem2 = new JMenuItem("test2");
+        
+        JMenuItem menuItem2 = new JMenuItem("icon2");
+        menuItem2.setIcon(icon2);
         menuItem2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                System.out.println("test2");
+                sys.setIcon(icon2);
+                System.out.println("icon2");
             }
         });
         menu.add(menuItem2);
 
-        Icon icon = loadImage("icon.png");
-
-        menuItem2.setIcon(icon);
+        JMenuItem menuItem3 = new JMenuItem("Quit");
+        menuItem3.setIcon(icon2);
+        menuItem3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                sys.close();
+                System.out.println("Quit");
+            }
+        });
+        menu.add(menuItem3);
 
         sys.addListener(ml);
         sys.setTitle("Java tool2");
-        sys.setIcon(icon);
+        sys.setIcon(icon1);
         sys.setMenu(menu);
         sys.show();
     }
